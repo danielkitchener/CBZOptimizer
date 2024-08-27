9083ec599abe1ae7ed219a792a20fabca6389bec
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2024-08-27 11:51:19 -0400

    fix: dependency issue with webp

--- converter/webp/webp_provider.go
+++ converter/webp/webp_provider.go
@@ -1,83 +1,22 @@
 package webp
 
 import (
-	"github.com/nickalie/go-binwrapper"
-	"github.com/nickalie/go-webpbin"
+	"github.com/belphemur/go-webpbin"
 	"image"
 	"io"
-	"os"
-	"path/filepath"
-	"runtime"
 )
 
 const libwebpVersion = "1.4.0"
 
-// NewCWebP creates new CWebP instance.
-func newCWebP(folder string) *webpbin.CWebP {
-	bin := &webpbin.CWebP{
-		BinWrapper: createBinWrapper(folder),
-	}
-	bin.ExecPath("cwebp")
-
-	return bin
-}
-
 func PrepareEncoder() error {
-	container := newCWebP(DefaultWebPDir)
+	webpbin.SetLibVersion(libwebpVersion)
+	container := webpbin.NewCWebP()
 	return container.BinWrapper.Run()
 }
-
-// DefaultWebPDir for downloaded browser. For unix is "$HOME/.cache/webp/bin",
-// for Windows it's "%APPDATA%\webp\bin"
-var DefaultWebPDir = filepath.Join(map[string]string{
-	"windows": filepath.Join(os.Getenv("APPDATA")),
-	"darwin":  filepath.Join(os.Getenv("HOME"), ".cache"),
-	"linux":   filepath.Join(os.Getenv("HOME"), ".cache"),
-}[runtime.GOOS], "webp", libwebpVersion, "bin")
-
 func Encode(w io.Writer, m image.Image, quality uint) error {
-	return newCWebP(DefaultWebPDir).
+	return webpbin.NewCWebP().
 		Quality(quality).
 		InputImage(m).
 		Output(w).
 		Run()
-}
-
-func createBinWrapper(dest string) *binwrapper.BinWrapper {
-	base := "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/"
-
-	b := binwrapper.NewBinWrapper().AutoExe()
-
-	b.Src(
-		binwrapper.NewSrc().
-			URL(base + "libwebp-" + libwebpVersion + "-mac-arm64.tar.gz").
-			Os("darwin").
-			Arch("arm64")).
-		Src(
-			binwrapper.NewSrc().
-				URL(base + "libwebp-" + libwebpVersion + "-mac-x86-64.tar.gz").
-				Os("darwin").
-				Arch("x64")).
-		Src(
-			binwrapper.NewSrc().
-				URL(base + "libwebp-" + libwebpVersion + "-linux-x86-32.tar.gz").
-				Os("linux").
-				Arch("x86")).
-		Src(
-			binwrapper.NewSrc().
-				URL(base + "libwebp-" + libwebpVersion + "-linux-x86-64.tar.gz").
-				Os("linux").
-				Arch("x64")).
-		Src(
-			binwrapper.NewSrc().
-				URL(base + "libwebp-" + libwebpVersion + "-windows-x64.zip").
-				Os("win32").
-				Arch("x64")).
-		Src(
-			binwrapper.NewSrc().
-				URL(base + "libwebp-" + libwebpVersion + "-windows-x86.zip").
-				Os("win32").
-				Arch("x86"))
-
-	return b.Strip(2).Dest(dest)
 }
--- go.mod
+++ go.mod
@@ -4,28 +4,29 @@ go 1.23.0
 
 require (
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
-	github.com/nickalie/go-binwrapper v0.0.0-20190114141239-525121d43c84
-	github.com/nickalie/go-webpbin v0.0.0-20220110095747-f10016bf2dc1
 	github.com/oliamb/cutter v0.2.2
 	github.com/samber/lo v1.47.0
 	github.com/spf13/cobra v1.8.1
+	github.com/thediveo/enumflag/v2 v2.0.5
 	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
 	golang.org/x/image v0.19.0
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/belphemur/go-binwrapper v0.0.0-20240827152605-33977349b1f0 // indirect
+	github.com/belphemur/go-webpbin v0.0.0-20240827154654-457b3fe588c4 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/dsnet/compress v0.0.1 // indirect
-	github.com/frankban/quicktest v1.14.6 // indirect
+	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mholt/archiver v3.1.1+incompatible // indirect
+	github.com/jfrog/archiver/v3 v3.6.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/pgzip v1.2.6 // indirect
 	github.com/nwaples/rardecode v1.1.3 // indirect
-	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
-	github.com/thediveo/enumflag/v2 v2.0.5 // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/text v0.17.0 // indirect
